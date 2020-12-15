ce162534f4067778296fc6acdb1d949fdc9bcf87
alexandr_baryshev <alexandr.baryshev@hotmail.com>
2020-12-15 19:33:05 +1000

    ...save

---
+++ lib/list.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'home.dart';
+
+
+class ListPageLayout extends StatelessWidget {
+  static String rout = 'ListPage';
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+          ListTop(),
+        List(),
+
+        ],
+      ),
+    ),);
+
+  }
+}
+// №№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№
+/// ▼ ▼ ▼ ▼ ▼ КАКАЯ-ТО ХУЕТА ↓ ↓ ↓ ↓ ↓ ↓
+
+
+class ListState extends ChangeNotifier {
+  int xUp = 0;
+
+  listTableUpdate() {
+    xUp += 1;
+    notifyListeners();
+  }
+}
+
+
+int qGlobal = 0;
+// ignore: must_be_immutable
+class List extends StatelessWidget {
+  int qLocal = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    print('>>>>>> qLocal  ${qLocal += 1}');
+
+    return
+      Expanded(child: Center(child:
+          Text("${context.select((ListState value) => value.xUp) == null ? '' : context.select((
+          ListState value) => value.xUp)}")));
+  }
+}
+
+
+class ListTop extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 10, right: 20),
+      height: 80,
+
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            width: 50,
+            child: FlatButton(
+              height: 80,
+              padding: EdgeInsets.zero,
+              child: Icon(
+                Icons.keyboard_return,
+              ),
+              onPressed: () {
+                Navigator.pushNamed(context, HomePageTest.rout);
+              },
+            ),
+          ), // Назад
+          Container(
+            width: 50,
+            child: FlatButton(
+              height: 80,
+              padding: EdgeInsets.zero,
+              child: Icon(
+                Icons.update,
+              ),
+              onPressed: () {
+                context.read<ListState>().listTableUpdate();
+              },
+            ),
+          ),
+          Flexible(
+            child: Container(
+              padding: EdgeInsets.only(bottom: 5, top: 5),
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'name',
+                style: TextStyle(color: Colors.white, fontSize: 19),
+              ),
+            ), // NAME
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,100 +1,52 @@
+
 import 'package:flutter/material.dart';
-import 'web.dart';
-import 'parsing.dart';
-
-import 'dart:async';
-import 'dart:convert';
-
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-
 import 'package:provider/provider.dart';
+import 'list.dart';
+import 'home.dart';
 
-void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+
+void main() => runApp(ReportsMain());
+
+class ReportsMain extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<Tessst>(
-    create: (context) => Tessst(),
+
+    print(
+        '\n--------------------------------------------------------------------\n'
+            '   >>> >>> START APPLICATION !!!!!!!!!! START APPLICATION <<< <<<   \n'
+            '--------------------------------------------------------------------');
+
+
+
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<ListState>(create: (context) => ListState()),
+
+      ],
       child: MaterialApp(
-        home: MyHomePage(),
+        theme: ThemeData.light(),
+        initialRoute: HomePageTest.rout,
+        routes: {
+
+          HomePageTest.rout: (context) => HomePageTest(),
+          ListPageLayout.rout: (context) => ListPageLayout(),
+
+
+        },
+        onUnknownRoute: (RouteSettings settings) {
+          return MaterialPageRoute<void>(
+              settings: settings,
+              builder: (BuildContext context) {
+                return Scaffold(
+                  body: Center(
+                    child: Text('НЕИЗВЕСТНАЯ ОШИБКА'),
+                  ),
+                );
+              });
+        },
       ),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children:[
-          Expanded(child: TextField(
-            onChanged: (val) {
-
-              context.read<Tessst>().setN(x: val);
-
-            },
-          )),
-          Expanded(
-            child: FutureBuilder<List<Report>>(
-              future:context.watch<Tessst>().fetchReports(/*http.Client()*/),
-              builder: (context, snapshot) {
-                if (snapshot.hasError) print(snapshot.error);
-
-                return snapshot.hasData
-                    ? ReportsList(reports: snapshot.data)
-                    : Center(child: CircularProgressIndicator());
-              },
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-
-
-
-
-
-
-
-
-
-class ReportsList extends StatelessWidget {
-  final List<Report> reports;
-
-
-  ReportsList({this.reports});
-
-  @override
-  Widget build(BuildContext context) {
-
-
-
-    return ListView.builder(
-
-      itemCount: reports.length,
-      itemBuilder: (context, index) {
-        return ListTile(
-          leading: Text(reports[index].serialNumber),
-          title: Text(reports[index].opisanieText),
-          subtitle:  Text(reports[index].id),
-          dense: true,
-          onTap: () {
-              print(reports[index].id);
-              // на страницу ввода, передается id и данный грузятся по ...GetById
-              // osnovanieText - сделать проверку на null
-          },
-        );
-      },
     );
   }
 }
