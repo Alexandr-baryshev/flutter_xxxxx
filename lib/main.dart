e86f7716c51d5557f4c355e2562c681bf21991a7
alexandr_baryshev <alexandr.baryshev@hotmail.com>
2021-01-23 16:21:00 +1000

    начало

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,55 @@
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'list.dart';
+
+
+
+
 
 
 void main() => runApp(ReportsMain());
 
 class ReportsMain extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    print('\n--------------------------------------------------------------------\n'
+    String funcName = 'Widget build';
+
+    print(
+        '\n--------------------------------------------------------------------\n'
         '   >>> >>> START APPLICATION !!!!!!!!!! START APPLICATION <<< <<<   \n'
         '--------------------------------------------------------------------');
+    // Logger.loggerOnOff = true;
+    // Logger.events(widget: '${context.widget}', func: funcName, event: '');
+
+
+
 
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider<TextWidgetState>(create: (context) => TextWidgetState()),
+        // ChangeNotifierProvider<ListTableState>(create: (context) => ListTableState()),
+
       ],
       child: MaterialApp(
         theme: ThemeData.light(),
-        initialRoute: Home.rout,
+        //initialRoute: RoutNameStorage.home,
         routes: {
-          Home.rout: (context) => Home(),
-          ListPage.rout: (context) => ListPage(),
+
+          // RoutNameStorage.home: (context) => HomePageLayout(),
+
+
         },
-      ),
-    );
-  }
-}
-
-class Home extends StatelessWidget {
-  static String rout = 'HomePage';
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ListTile(
-          title: Text('GO NEXT'),
-          onTap: () {
-            Navigator.pushNamed(context, ListPage.rout);
-          },
-        ),
+        onUnknownRoute: (RouteSettings settings) {
+          return MaterialPageRoute<void>(
+              settings: settings,
+              builder: (BuildContext context) {
+                return Scaffold(
+                  body: Center(
+                    child: Text('НЕИЗВЕСТНАЯ ОШИБКА'),
+                  ),
+                );
+              });
+        },
       ),
     );
   }
